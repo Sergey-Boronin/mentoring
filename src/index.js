50ca2443e1064a396598e34a372cdec5413d2707
Sergey Boronin <bo-ronin@yandex.ru>
2022-01-07 12:46:43 +0300

    добавил урок №3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './lesson-2/App';
+import '../src/lesson-3/style.css'
+import App from './lesson-3/App';
 
 
 ReactDOM.render(
