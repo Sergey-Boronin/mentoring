bd578a75f41157afdc94ba4d828819de602e69bc
Sergey Boronin <bo-ronin@yandex.ru>
2022-02-10 21:04:43 +0300

    добавил туториал-6

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/lesson-3/style.css'
-import App from './tutorial-5/App'
+import App from './tutorial-6/App'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 ReactDOM.render(
